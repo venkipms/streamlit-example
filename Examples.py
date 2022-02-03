4a53e033e1f7432d80624f7ef82e0f57d1149736
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-03 16:28:51 +0530

    Update Examples.py

--- Examples.py
+++ Examples.py
@@ -49,3 +49,14 @@ chart_data = pd.DataFrame(
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+# Example 6
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+map_data = pd.DataFrame(
+    np.random.randn(100, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
