68f0caaed75cef6ba7948ca2a1e41ea6b8cfc557
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-03 15:35:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-df
+st.write("First Attempt in using Data to create Table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
