b717ad816712e30a6ddb24d7cf3e4ec2f1cafcf7
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-03 16:51:04 +0530

    Update Examples.py

--- Examples.py
+++ Examples.py
@@ -66,3 +66,15 @@ import streamlit as st
 
 X = st.slider('x')  # It's a widget
 st.write('Slider Value =', X, 'Squared Value = ', X * X)
+
+# Example 7
+import streamlit as st
+
+st.text_input("Name", key="name")
+st.text_input("Age", key='age')
+st.text_input("University", key='univ')
+
+# You can access the value at any point with:
+st.session_state.name
+st.session_state.age
+st.session_state.univ
