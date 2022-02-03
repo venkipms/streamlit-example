d0925a6e5ba585bc48bb78a1e0f8b0f3eb389542
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-03 17:05:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 df = pd.DataFrame({'First Column': [1, 2, 3, 4],
                     'Second Column': [10, 20, 30, 40] })
 
-option = st.selectbox('Number You Like Best', df['first column'])
+option = st.selectbox('Number You Like Best', df['First Column'])
 
 'Your Selection: ', option
 
