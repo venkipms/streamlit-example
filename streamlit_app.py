c09fc48dff9fe6a0275594937eb91166cf866c64
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-03 16:49:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
-st.text_input("Your Name", key="name")
+
+st.text_input("Name", key="name")
+st.text_input("Age", key='age')
+st.text_input("University", key='univ')
 
 # You can access the value at any point with:
 st.session_state.name
+st.session_state.age
+st.session_state.univ
+
+
