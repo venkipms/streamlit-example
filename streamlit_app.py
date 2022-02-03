9964cc188fe56f214252898316732a456fc6772d
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-03 16:55:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,10 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
-st.text_input("Name", key="name")
-st.text_input("Age", key='age')
-st.text_input("University", key='univ')
-
-# You can access the value at any point with:
-st.session_state.name
-st.session_state.age
-st.session_state.univ
+if st.checkbox('Show Dataframe'):
+    chart_data = pd.DataFrame(np.random.randn(20, 3),
+                                    columns=['a', 'b', 'c'])
+    chart_data
 
 
