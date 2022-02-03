0d97f1c8109588b6ea515769bccec88df354dbb3
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-03 17:02:25 +0530

    Update Examples.py

--- Examples.py
+++ Examples.py
@@ -78,3 +78,15 @@ st.text_input("University", key='univ')
 st.session_state.name
 st.session_state.age
 st.session_state.univ
+
+# Example 8
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+if st.checkbox('Show Dataframe'):
+    chart_data = pd.DataFrame(np.random.randn(20, 3),
+                                    columns=['a', 'b', 'c'])
+    chart_data
+
+    
