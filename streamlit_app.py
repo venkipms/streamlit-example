58b069af6c9e3ef849bf59a824fb5cc3f969011f
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-03 15:56:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 import streamlit as st
-import pandas as pd
+import numpy as np
 
-st.write("First Attempt in using Data to create Table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
