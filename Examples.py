8a418bc9580491ef59159e70d0dafe7055f17d7a
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-03 16:40:31 +0530

    Update Examples.py

--- Examples.py
+++ Examples.py
@@ -60,3 +60,9 @@ map_data = pd.DataFrame(
     columns=['lat', 'lon'])
 
 st.map(map_data)
+
+# Example 7
+import streamlit as st
+
+X = st.slider('x')  # It's a widget
+st.write('Slider Value =', X, 'Squared Value = ', X * X)
