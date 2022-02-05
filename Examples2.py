986f6808fefa36fb4da07a55df91486c517e909b
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-05 20:53:03 +0530

    Create Examples2.py

---
+++ Examples2.py
@@ -0,0 +1,11 @@
+# Example 1
+import streamlit as st
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(' Option for Personal contact:',
+                                            ('Email', 'Landline', 'Mobile phone'))
+"Personal Contact Selected: ", add_selectbox
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider('Select a range of values', 0, 100)
+"Range Selected: ", add_slider
