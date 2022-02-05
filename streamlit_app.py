edaddf76ffe319e30ec7e0e76f98221c8d10cf0b
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-05 21:50:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,7 @@ data_load_state.text("Done! Data Loading")
 if st.checkbox('Show Raw Data'):
     st.subheader('Raw Data')
     st.write(data)
+
+st.subheader('Number of Pickups by Hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
