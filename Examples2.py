b2288cb26457d97899ece51670a0874be2d35cc7
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-05 21:03:01 +0530

    Update Examples2.py

--- Examples2.py
+++ Examples2.py
@@ -9,3 +9,16 @@ add_selectbox = st.sidebar.selectbox(' Option for Personal contact:',
 # Add a slider to the sidebar:
 add_slider = st.sidebar.slider('Select a range of values', 0, 100)
 "Range Selected: ", add_slider
+
+# Example 2
+import streamlit as st
+
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('SUBMIT')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio('Programming Language',
+        ("Python", "Java", "Kotlin", "Scala"))
+    st.write(f"Language {chosen} Selected")
