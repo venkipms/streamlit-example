73bcf14d88087b40ef075e3d55f9c055bb28a6d5
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-05 20:57:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
-# Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(' Option for Personal contact:',
-                                            ('Email', 'Landline', 'Mobile phone'))
-"Personal Contact Selected: ", add_selectbox
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('SUBMIT')
 
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider('Select a range of values', 0, 100)
-"Range Selected: ", add_slider
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
