5f1a7828dd6fb9093b89adb380066e75b8169fba
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-05 21:52:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,7 @@ if st.checkbox('Show Raw Data'):
 st.subheader('Number of Pickups by Hour')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
