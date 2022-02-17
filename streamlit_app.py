7c50d568c0aa08909d779dd4246ece0d11d39f49
Venketesh Palanisamy <67791301+venkipms@users.noreply.github.com>
2022-02-17 12:21:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from streamlit.components.v1 import iframe
 st.set_page_config(layout="centered", page_icon="🎓", page_title = "Certificate Generator")
 col1, col2 = st.columns(2)
 
-col1.image("vit_3.png", use_column_width=True)
+col1.image("vit_3.png", width=100)
 
 col2.title("🎓 Certificate Generator")
 
